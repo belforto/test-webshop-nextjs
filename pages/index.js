aed5a1910c2771c90ecd2c0be4e99f1e8a76055d
belforto <marko.ognjenovich@gmail.com>
2020-12-02 22:10:27 +0100

    zustand store added

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,35 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header/>
 
-      <main className={styles.main}>
+      <main className={[styles.container]}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
        
-<div class="bg-gray-50">
-  <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-    <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-      <span class="block">Ready to dive in?</span>
-      <span class="block text-indigo-600">Start your free trial today.</span>
+<div className="bg-primary">
+  <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+    <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+      <span className="block ">Ready to dive in?</span>
+      <span className="block text-indigo-600">Start your free trial today.</span>
     </h2>
-    <div class="mt-8 lex lg:mt-0 lg:flex-shrink-0">
-      <div class="inline-flex rounded-md shadow">
-        <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
+    <div className="mt-8 lex lg:mt-0 lg:flex-shrink-0">
+      <div className=" inline-flex rounded-md shadow bg-primary">
+        <a href="#" className="text-indigo-light inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
           Get started
         </a>
       </div>
-      <div class="ml-3 inline-flex rounded-md shadow">
-        <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
+      <div className="ml-3 inline-flex rounded-md shadow">
+        <a href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
           Learn more
         </a>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
+
+
 module.exports = {
-  purge:{
-      //include only used classes 
-      //after build 
-      enabled:true, 
-      content:['./pages/**/*.js']} ,
+  
+  purge: {
+    //include only used classes
+    //after build
+    enabled: false,
+    content: ["./pages/**/*.js"]
+  },
+  darkMode: false, // can be 'media' or 'class'
+  // Customizations specific to this project would go here
   theme: {
-    extend: {}
+    extend: {
+        colors: {
+            blue: {
+              light: '#85d7ff',
+              DEFAULT: '#1fb6ff',
+              dark: '#009eeb',
+            },
+    }
+  },
+},
+
+  variants: {
+    extend: {
+      //   borderColor: ['focus-visible'],
+      //   opacity: ['disabled'],
+    }
   },
   variants: {},
   plugins: []
