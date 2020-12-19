139382f10176a161164a840ec01f39774a21ecb0
belforto <marko.ognjenovich@gmail.com>
2020-12-19 18:10:43 +0100

    dynamic import tested on /contact

---
+++ components/Test2.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Test2() {
+    return (
+        <div>
+      <figure class="bg-blue-500 rounded-xl p-8">
+        <img
+          class="w-32 h-32 rounded-full"
+          src="https://tailwindcss.com/_next/static/media/sarah-dayan.a8ff3f1095a58085a82e3bb6aab12eb2.jpg"
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div class="pt-6 space-y-4">
+          <blockquote>
+            <p class="text-lg">“Hey I am Contact person Number 2”</p>
+          </blockquote>
+          <figcaption>
+            <div>Mara Dayan</div>
+            <div>Staff Engineer, Canada</div>
+          </figcaption>
+        </div>
+      </figure>
+    </div>
+    )
+}
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import {useEffect,useState,React} from 'react'
 import Header from '../../components/Header'
 
+import dynamic from 'next/dynamic'
+
+
+
+
+
+
 export default function Contact() {
+    const [componentName, setComponentName] = useState("Test1")
+
+    useEffect(() => {
+       
+    }, [componentName])
+
+    const DynamicComponentWithNoSSR = dynamic(
+        () => import("../../components/"+componentName),
+        { ssr: false }
+      )
   
   
     return (
         <div>
             <Header/>
             Contact Us
-
+            <DynamicComponentWithNoSSR />
+            <button onClick={()=> setComponentName("Test2")} > Dynamically import the component</button>
             
         </div>
     )
 }
+
+
